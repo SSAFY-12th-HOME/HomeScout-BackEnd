fdda007251ac3e2cbcff9bc59814ab45f04c9e52
taeho <rktlrhrl531@naver.com>
2024-11-06 17:09:13 +0900

    [init]

---
+++ src/test/java/com/ssafy/homescout/HomescoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.homescout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomescoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
